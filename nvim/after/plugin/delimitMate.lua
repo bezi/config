04d0faa5f4aff63b4a967559debb5731ea9814e1
Oscar Bezi <oscar.bezi@datadoghq.com>
2023-07-14 13:58:39 -0400

    Add after directory for nvim.

---
+++ nvim/after/plugin/delimitMate.lua
@@ -0,0 +1,6 @@
+--- will close parens inside of quotes
+vim.g.delimitMate_expand_inside_quotes = 1
+
+--- hitting enter and space inside of delimiters works more nicely
+vim.g.delimitMate_expand_cr = 2
+vim.g.delimitMate_expand_space = 1
