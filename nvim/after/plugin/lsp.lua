853d3bb2a907ae3c8d1c6eb33f8d743069864645
Oscar Bezi <oscar@bezi.io>
2025-10-18 13:03:13 -0400

    I think that fixes it?

--- nvim/after/plugin/lsp.lua
+++
@@ -1,9 +0,0 @@
-local lsp = require("lsp-zero")
-
-lsp.preset("recommended")
-
-lsp.on_attach(function(_client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
-end)
-
-lsp.setup()
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -19,8 +19,10 @@ return {
 	},
 
 	-- Helper for telescope sorting
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
-
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+	},
 
 	-- Color scheme
 	{
@@ -36,25 +38,6 @@ return {
 		build = ":TSUpdate",
 	},
 
-	-- LSP Support
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		dependencies = {
-			{ "neovim/nvim-lspconfig" },
-			{
-				"williamboman/mason.nvim",
-				build = ":MasonUpdate",
-			},
-			{ "williamboman/mason-lspconfig.nvim" },
-
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-		},
-	},
-
 	-- Run linters
 	{ "mfussenegger/nvim-lint" },
 
@@ -63,6 +46,94 @@ return {
 		"stevearc/conform.nvim",
 		config = function()
 			require("conform").setup()
+		end,
+	},
+
+	-- LSPs
+	-- Mason: install servers
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "pyright", "vtsls" },
+				automatic_installation = true,
+			})
+		end,
+	},
+
+	-- LSPconfig & native LSP API
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- common on_attach + capabilities
+			local on_attach = function(client, bufnr)
+				local bufopts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, bufopts)
+			end
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+			if cmp_ok then
+				capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+			end
+
+			local servers = { "pyright", "vtsls" }
+			for _, lsp in ipairs(servers) do
+				vim.lsp.config(lsp, {
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+				vim.lsp.enable(lsp)
+			end
+		end,
+	},
+
+	-- CMP + completion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+						else
+							cmp.complete() -- trigger completion menu
+						end
+					end,
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+			})
 		end,
 	},
 }
